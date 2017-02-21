b0f33a529a42151806b7b0a21f815360f962cfbe
theheena <heena34bawa@outlook.com>
2017-02-21 18:39:29 +0530

    commit

--- Dynamic Programming/candies.js
+++ Dynamic Programming/candies.js
@@ -1,18 +1,38 @@
 function processData(input) {
     //Enter your code here
-    var temp = 0;
-    var count = 0;
-    for(i=1;i<=input[0]; i++){
-        if(input[i] == temp){
-            count = count + 2;
-        }
-        else{
-            count++;
-        }
-        temp = input[i];
+    let temp = 0;
+    let count = [];
+    input = input.split("\n");
+    input.map(Number);
+    for(let i=1;i<=input[0]; i++){
+        count[i] = 1;
     }
-    console.log(count);
+    for(let i=1;i<=input[0]-1;i++){
+        if(input[i+1]>input[i]){
+            count[i+1]  = count[i] + 1;
+        }
+        if(input[i] > input[i+1]){
+            if(count[i]<=count[i+1]){
+                count[i] = count[i+1] + 1;
+            }
+        }
+    }
+    for(let i = 1;i<=input[0];i++){
+        temp = temp + count[i];
+        //console.log(count[i]);
+    }
+    console.log(temp);
 }
 
-input = [3,1,2,2];
+input = `10
+2
+4
+2
+6
+1
+7
+8
+9
+2
+1`;
 processData(input); 
